6be2835daa3c0fa243c9f770fba26642b532dd01
jihyekwak <jihyekwak53@gmail.com>
2022-03-31 11:59:15 -0700

    added url path and view for travel list,  and created travel_list.html

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
+    path('travels/', views.Travel_List.as_view(), name='travel_list'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,9 @@
+from re import template
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.http import HttpResponse
+from .models import Travel
 
 # Create your views here.
 
@@ -10,3 +12,11 @@ class Home(TemplateView):
 
     # def get(self, request):
     #     return HttpResponse("Travel Planner")
+
+class Travel_List(TemplateView):
+    template_name = 'travel_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['travels'] = Travel.objects.all()
+        return context
