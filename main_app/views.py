67e5ad4842a7d04856b0a17f02cce297f4d220f5
jihyekwak <jihyekwak53@gmail.com>
2022-03-31 01:27:59 -0700

    created home.html and update home view

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.base import TemplateView
 from django.http import HttpResponse
 
 # Create your views here.
 
-class Home(View):
+class Home(TemplateView):
+    template_name = 'home.html'
 
-    def get(self, request):
-        return HttpResponse("Travel Planner")
+    # def get(self, request):
+    #     return HttpResponse("Travel Planner")
