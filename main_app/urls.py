67d1ffcef0475cc94edfe8b9001a7b2ec148dbf0
jihyekwak <jihyekwak53@gmail.com>
2022-04-02 13:01:07 -0700

    add urls for destinations

--- main_app/urls.py
+++ main_app/urls.py
@@ -12,4 +12,10 @@ urlpatterns = [
     path('travels/<int:pk>/itineraries/new', views.Itinerary_Create.as_view(), name='itinerary_create'),
     path('travels/<int:pk>/itineraries/<int:itinerary_id>/update', views.itinerary_update, name='itinerary_update'),
     path('travels/<int:pk>/itineraries/<int:itinerary_id>/delete', views.itinerary_delete, name='itinerary_delete'),
+
+    path('destinations/', views.destination_list, name='travel_list'),
+    path('destinations/new', views.Destination_Create.as_view(), name='travel_create'),
+    path('destinations/<int:destination_id>', views.Destination_Detail.as_view(), name='travel_detail'),
+    path('destinations/<int:destination_id>/update', views.Destination_Update.as_view(), name='travel_update'),
+    path('destinations/<int:destination_id>/delete', views.Destination_Delete.as_view(), name='travel_delete'),
 ]
